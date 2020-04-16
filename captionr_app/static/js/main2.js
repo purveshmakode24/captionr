2350300a47aa5e29b41563b09a618592a1c54ada
Purvesh Makode <makodepurvesh@gmail.com>
2020-04-16 13:43:10 +0530

    Add facebot onmousehover anim

--- captionr_app/static/js/main2.js
+++
@@ -1,9 +0,0 @@
-// (function(n, t, i) {
-//     var r, u = n.getElementsByTagName(t)[0];
-//     n.getElementById(i) || (r = n.createElement(t), r.id = i, r.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3", u.parentNode.insertBefore(r, u))
-// })(document, "script", "facebook-jssdk");
-// ! function(n, t, i) {
-//     var r, u = n.getElementsByTagName(t)[0],
-//         f = /^http:/.test(n.location) ? "http" : "https";
-//     n.getElementById(i) || (r = n.createElement(t), r.id = i, r.src = f + "://platform.twitter.com/widgets.js", u.parentNode.insertBefore(r, u))
-// }(document, "script", "twitter-wjs")
