e52dbbfdeb9d441def16bb365932969161497f39
Purvesh Makode <makodepurvesh@gmail.com>
2020-04-21 03:27:37 +0530

    Fix outlines to none. add few updates

--- captionr_app/static/js/main.js
+++ captionr_app/static/js/main.js
@@ -2,7 +2,8 @@
 
 // loading button
 function analyzing() {
-    document.getElementById("analyze").innerHTML = "<i  class='fa fa-spin fa-spinner'></i> Analyzing...";
+    // document.getElementById("analyze").innerHTML = "<i  class='fa fa-spin fa-spinner'></i> Analyzing...";
+    document.getElementById("analyze").innerHTML = "<i class='fas fa-spinner fa-pulse'></i> Analyzing...";
     document.getElementById("analyze").disabled = true;
     // document.getElementById("analyze").classList.add("no-hover");
     document.getElementById("analyze").style.backgroundColor = "#bbb";
