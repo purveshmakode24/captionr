ebda36822294e692b831e2a36057e3d1c4b87274
Purvesh Makode <makodepurvesh@gmail.com>
2020-04-22 15:37:50 +0530

    Commented whitenoise and STATIC_ROOT

--- captionr/settings.py
+++ captionr/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'captionr.urls'
@@ -107,7 +107,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
